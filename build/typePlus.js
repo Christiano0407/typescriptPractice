8895ffa58cf4ad3c1882b86bbda0f9f6536e4b57
Christiano0407 <placebo0704@gmail.com>
2021-11-08 20:49:32 -0600

    typescript06

--- build/typePlus.js
+++ build/typePlus.js
@@ -2,3 +2,11 @@
 var result;
 result = "Yes";
 console.log(result);
+//Array
+var list = [1, 2, 3, 4, 5];
+console.log(list);
+//string>
+var listTwo = ["Pamme", "Luisa", "Mau", "Alexa"];
+console.log(listTwo);
+var student = ["Chris", 4];
+console.log(student);
--- typePlus.ts
+++ typePlus.ts
@@ -3,3 +3,13 @@ let result: answer;
 
 result = "Yes";
 console.log(result);
+
+//Array
+let list: number[] = [1, 2, 3, 4, 5];
+console.log(list);
+//string>
+let listTwo: Array<string> = ["Pamme", "Luisa", "Mau", "Alexa"];
+console.log(listTwo);
+
+let student: [string, number] = ["Chris", 4];
+console.log(student);
